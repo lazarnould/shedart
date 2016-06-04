a8cdd1b5894a12e6eb8dd0fdd068cd5b53a5c621
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-06-04 18:01:05 +0200

    sign in with facebook final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,4 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
-
-  def after_sign_in_path_for(resource_or_scope)
-    if resource.sign_in_count == 1
-      new_profile_path
-    else
-      profile_path
-    end
-  end
 end
