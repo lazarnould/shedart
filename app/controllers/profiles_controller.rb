e7d3024c652d8174808b217efdf6e8faf7658d2a
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-09-14 11:39:18 +0200

    profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,34 @@
 class ProfilesController < ApplicationController
+  before_action :find_profile, only: [:edit, :update, :show]
 
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    @profile.user_id = current_user
+    if @profile.save
+      flash[:success] = "You're profile has been created"
+      redirect_to profile_path(@profile)
+    else
+      flash[:error] = "An error occured, please try again"
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @profile.update(profile_params)
+    redirect_to profile_path(@profile)
   end
 
   def show
+    if @profile.nil?
+      redirect_to new_profile_path
+    end
   end
 
   private
