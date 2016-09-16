e48da04034c05594b6abe64fcbb453e2a45ed115
Laszlo Arnould <laszlo.arnould@gmail.com>
2016-09-16 15:38:45 +0200

    mailboxer installed + conversations controller/views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
+
+  helper_method :mailbox
+
+  private
+
+  def mailbox
+    @mailbox ||= current_user.mailbox
+  end
+
+  def conversation
+    @conversation ||= mailbox.conversations.find(params[:id])
+  end
+
 end
